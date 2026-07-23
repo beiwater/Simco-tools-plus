677605abfec558d1f8935a0809608480fc187d8a
beiwater <huanhai2333@gmail.com>
2026-07-23 17:51:40 +1000

    chore: disable optional features by default

--- components/customExecutivesIcon.js
+++ components/customExecutivesIcon.js
@@ -6,7 +6,7 @@ class customExecutivesIcon extends BaseComponent {
     super();
     this.name = "自定义高管头像";
     this.describe = "在公司高管页面/被挖角页面/公司主页面/百科页面/科研页面将指定公司高管的头像替换."
-    this.enable = true;
+    this.enable = false;
     this.tagList = ['个性化','高管'];
   }
   indexDBData = {
